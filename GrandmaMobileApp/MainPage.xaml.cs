17abe9f7350bbfdacb8babf6c2d228358b61d55f
Yaser Moradi <ysmoradi@outlook.com>
2021-01-03 23:41:05 +0330

    Initial contact read code

--- GrandmaMobileApp/MainPage.xaml.cs
+++ GrandmaMobileApp/MainPage.xaml.cs
@@ -1,12 +1,24 @@
-﻿namespace GrandmaMobileApp
+﻿using System.Collections.ObjectModel;
+using Xamarin.Essentials;
+
+namespace GrandmaMobileApp
 {
     public partial class MainPage 
     {
+        public ObservableCollection<Contact> Contacts { get; set; } = new ObservableCollection<Contact>();
+
         public MainPage()
         {
-            InitializeComponent();
+            InitializeComponent();            
         }
 
-        public string Test { get; set; } = "Test";
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            await Permissions.RequestAsync<Permissions.ContactsRead>();
+
+            Contacts = new ObservableCollection<Contact>(await Xamarin.Essentials.Contacts.GetAllAsync(default));
+        }
     }
 }
